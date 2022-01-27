8322af813ed529818715863a6eb55f71020fdb48
Nikita Vandyshev <nekitvand@gmail.com>
2022-01-27 11:14:13 +0300

    init commit

---
+++ helpers/find_actions.py
@@ -0,0 +1,33 @@
+from typing import List
+from typing import Tuple
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class FindAction:
+
+    def __init__(self, driver):
+        self.driver = driver
+        self.test_url = None
+
+    def find(self, locator, timeout=10):
+        # type: (Tuple[str, str], int) -> WebElement
+        return WebDriverWait(self.driver, timeout, poll_frequency=1). \
+            until(EC.presence_of_element_located(locator))
+
+    def find_visible(self, locator, timeout=10):
+        # type: (Tuple[str, str], int) -> WebElement
+        return WebDriverWait(self.driver, timeout, poll_frequency=1). \
+            until(EC.visibility_of_element_located(locator))
+
+    def finds(self, locator, timeout=10):
+        # type: (Tuple[str, str], int) -> List[WebElement]
+        return WebDriverWait(self.driver, timeout, poll_frequency=1). \
+            until(EC.presence_of_all_elements_located(locator))
+
+    def finds_visible(self, locator, timeout=10):
+        # type: (Tuple[str, str], int) -> List[WebElement]
+        return WebDriverWait(self.driver, timeout, poll_frequency=1). \
+            until(EC.visibility_of_all_elements_located(locator),
+                  message=f"Can't find elements by locator {locator}")
